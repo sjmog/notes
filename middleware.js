0bbb709398bab49aa2d11883b1d025c361b5dd52
Sam Morgan <s_morgan@me.com>
2023-09-17 12:34:27 +0200

    Initial commit

--- middleware.js
+++ middleware.js
@@ -1,9 +1,7 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server'
-
-export async function middleware(req: NextRequest) {
+export async function middleware(req) {
   const res = NextResponse.next()
 
   // Create a Supabase client configured to use cookies
